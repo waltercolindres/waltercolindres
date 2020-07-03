7bcf1767d73c0e78bd7647b0d43b507eb9d58c6f
W <veridian@VeridiaDynamics.hsd1.fl.comcast.net>
2020-07-03 12:16:51 -0400

    twael

--- components/page-body-copy.js
+++ components/page-body-copy.js
@@ -3,45 +3,48 @@ import Breadcrumbs from "components/breadcrumbs";
 import { INLINES } from "@contentful/rich-text-types";
 
 function PageBodyCopy({ content }) {
-  console.log("content");
-  console.log(content);
-  console.log("content");
-
   // https://github.com/contentful/rich-text/issues/61
   // https://github.com/contentful/rich-text/issues/88
 
-  const renderImages = {
-    renderNode: {
-      "embedded-asset-block": (node) => {
-        const file = node.data.target.fields.file.url;
-        const title = node.data.target.fields.title;
-        const alt = node.data.target.fields.alt;
-        return (
-          <img className="reviews-image" src={file} alt={file} title={title} />
-        );
-      },
-      [INLINES.HYPERLINK]: (node) => {
-        if (node.data.uri.includes("player.vimeo.com/video")) {
+  if (content) {
+    const renderImages = {
+      renderNode: {
+        "embedded-asset-block": (node) => {
+          const file = node.data.target.fields.file.url;
+          const title = node.data.target.fields.title;
+          const alt = node.data.target.fields.alt;
           return (
-            <div className="videoWrapper">
-              <iframe
-                title="Unique Title 001"
-                src={node.data.uri}
-                frameBorder="0"
-                allowFullScreen
-              />
-            </div>
+            <img
+              className="reviews-image"
+              src={file}
+              alt={file}
+              title={title}
+            />
           );
-        }
+        },
+        [INLINES.HYPERLINK]: (node) => {
+          if (node.data.uri.includes("player.vimeo.com/video")) {
+            return (
+              <div className="videoWrapper">
+                <iframe
+                  title="Unique Title 001"
+                  src={node.data.uri}
+                  frameBorder="0"
+                  allowFullScreen
+                />
+              </div>
+            );
+          }
+        },
+        // [BLOCKS.PARAGRAPH]: (node, children) =>
+        //   node.content.some((childNode) => childNode.nodeType === `p`) ? (
+        //     children
+        //   ) : (
+        //     <div className="blogpost-text">{children}</div>
+        //   ),
       },
-      // [BLOCKS.PARAGRAPH]: (node, children) =>
-      //   node.content.some((childNode) => childNode.nodeType === `p`) ? (
-      //     children
-      //   ) : (
-      //     <div className="blogpost-text">{children}</div>
-      //   ),
-    },
-  };
+    };
+  }
 
   let x;
 
