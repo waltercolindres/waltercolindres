938f3f561106cbab37bc98b97836a92d016faa1e
W <veridian@VeridiaDynamics.hsd1.fl.comcast.net>
2020-07-03 09:54:13 -0400

    comment out preview

--- components/all-sections.js
+++ components/all-sections.js
@@ -1,8 +1,7 @@
-import PostPreview from "./post-preview";
 import SectionList from "./section-list";
 
 export default function AllSections({ posts }) {
-  console.log(posts);
+  // console.log(posts);
 
   const Sections = Object.keys(posts).map(function (key) {
     return <SectionList key={key} posts={posts[key]} title={key} />;
--- components/section-list.js
+++ components/section-list.js
@@ -1,9 +1,6 @@
 import PostPreview from "./post-preview";
 
 export default function SectionList({ posts, title }) {
-  console.log("LLLLLLLLLLL");
-  console.log(posts);
-  console.log("LLLLLLLLLLL");
   return (
     <div className="projects-list">
       <h2>{title}</h2>
