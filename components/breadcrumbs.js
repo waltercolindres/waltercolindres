ef925d51129bf0e9a512af4b53dba083001f3dd0
W <veridian@VeridiaDynamics.hsd1.fl.comcast.net>
2020-07-03 10:16:14 -0400

    sdsad

--- components/breadcrumbs.js
+++ components/breadcrumbs.js
@@ -1,4 +1,40 @@
 function Breadcrumbs({ title }) {
+  if (typeof window !== "undefined" && typeof document !== "undefined") {
+    function debounce(func, wait, immediate) {
+      var timeout;
+      return function () {
+        var context = this,
+          args = arguments;
+        var later = function () {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
+
+    function readingPosition() {
+      var scrollTop =
+        document.documentElement["scrollTop"] || document.body["scrollTop"];
+      var scrollBottom =
+        (document.documentElement["scrollHeight"] ||
+          document.body["scrollHeight"]) -
+        document.documentElement.clientHeight;
+      var scrollPercent = (scrollTop / scrollBottom) * 100 + "%";
+      document
+        .getElementById("_progress")
+        .style.setProperty("--scroll", scrollPercent);
+    }
+
+    // https://medium.com/@nilayvishwakarma/build-a-scroll-progress-bar-with-vanilla-js-in-10-minutes-or-less-4ba07e2554f3
+    document.addEventListener("scroll", debounce(readingPosition, 10), {
+      passive: true,
+    });
+  }
+
   return (
     <>
       <nav className="breadcrumb">
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -20,41 +20,7 @@ export default function Post({ post, preview }) {
   if (!router.isFallback && !post) {
     return <ErrorPage statusCode={404} />;
   }
-  if (typeof window !== "undefined" && typeof document !== "undefined") {
-    function debounce(func, wait, immediate) {
-      var timeout;
-      return function () {
-        var context = this,
-          args = arguments;
-        var later = function () {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-      };
-    }
 
-    function readingPosition() {
-      var scrollTop =
-        document.documentElement["scrollTop"] || document.body["scrollTop"];
-      var scrollBottom =
-        (document.documentElement["scrollHeight"] ||
-          document.body["scrollHeight"]) -
-        document.documentElement.clientHeight;
-      var scrollPercent = (scrollTop / scrollBottom) * 100 + "%";
-      document
-        .getElementById("_progress")
-        .style.setProperty("--scroll", scrollPercent);
-    }
-
-    // https://medium.com/@nilayvishwakarma/build-a-scroll-progress-bar-with-vanilla-js-in-10-minutes-or-less-4ba07e2554f3
-    document.addEventListener("scroll", debounce(readingPosition, 10), {
-      passive: true,
-    });
-  }
   return (
     <>
       <Layout preview={preview}>
@@ -72,7 +38,7 @@ export default function Post({ post, preview }) {
 
               <div className="sticky">
                 <h1>{post.title}</h1>
-                <Breadcrumbs title={post.title} />
+                {/* <Breadcrumbs title={post.title} /> */}
               </div>
 
               <div className="mY2">META</div>
