5fe61d57aef0a5af2cc121fe13c7965e5866ae7c
W <veridian@VeridiaDynamics.hsd1.fl.comcast.net>
2020-07-18 19:21:36 -0400

    fixes

--- components/post-preview.js
+++ components/post-preview.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function PostPreview({ title, slug, postType }) {
   return (
     <li>
-      <Link as={`/${postType}/${slug}`} href={`/${postType}/[slug]`}>
+      <Link as={`/${postType}/${slug}`} href={`/[postType]/[slug]`}>
         <a>{title}</a>
       </Link>
 
