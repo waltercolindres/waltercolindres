21a840160c5e5a8f90171b597cdb6cf3707b579b
W <veridian@VeridiaDynamics.hsd1.fl.comcast.net>
2020-07-18 23:40:28 -0400

    added meta

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,11 @@
 import Header from "components/header";
 import Footer from "components/footer";
+import Meta from "components/meta";
 
 function Layout({ preview, children }) {
   return (
     <>
+      <Meta />
       <Header />
       <div preview={preview ? false : undefined} className="main">
         {children}
