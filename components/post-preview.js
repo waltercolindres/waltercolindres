741e1b5fe18b3798b6fb5559b2dd1be15ea11e45
Walter Colindres <halo13design@gmail.com>
2021-06-23 17:41:08 -0700

    updated

--- components/post-preview.js
+++ components/post-preview.js
@@ -4,8 +4,9 @@ export default function PostPreview({ title, slug, postType, excerpt }) {
   return (
     <li>
       <Link as={`/${postType}/${slug}`} href={`/[postType]/[slug]`}>
-        <><a>{title}</a>/</>
+        <a>{title}</a>
       </Link>
+      <span>/</span>
       {/* <span className="indent"> */}
       <Link as={`/${postType}/${slug}`} href={`/[postType]/[slug]`}>
         <a className="unbold">{excerpt}</a>
