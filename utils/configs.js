ae556d63e4108b087ca9559ce938f7e46c1797cd
Walter Colindres <halo13design@gmail.com>
2021-06-25 13:24:13 -0700

    s

--- utils/configs.js
+++ utils/configs.js
@@ -3,5 +3,5 @@ export const TWITTER_CREATOR = "waltercolindres";
 export const SITE_TITLE = "Walter Colindres - Product Experience Leader";
 export const SITE_DESC =
   "Walter Colindres - Thoughts and Reviews that center around Product Experience and sometimes life.";
-export const SITE_IMG = "http://waltercolindres.com/me.png";
-export const SITE_URL = "waltercolindres.com";
+export const SITE_IMG = "https://waltercolindres.com/me.png";
+export const SITE_URL = "https://waltercolindres.com";
