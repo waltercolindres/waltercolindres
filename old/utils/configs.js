b524f907bb94f4dd8ecdbae78b5e57161083917d
waltercolindres <halo13design@gmail.com>
2026-03-29 08:30:18 -0700

    remove old stuff for now
    removed the old site.

--- old/utils/configs.js
+++
@@ -1,8 +0,0 @@
-export const TWITTER_SITE = "waltercolindres";
-export const TWITTER_CREATOR = "waltercolindres";
-export const SITE_TITLE = "Walter Colindres - Product Experience Leader";
-export const SITE_KEYWORDS = "product, experience, design, website, product management";
-export const SITE_DESC =
-  "Walter Colindres - Thoughts and Reviews that center around Product Experience and sometimes life.";
-export const SITE_IMG = "https://waltercolindres.com/me.png";
-export const SITE_URL = "https://waltercolindres.com";
